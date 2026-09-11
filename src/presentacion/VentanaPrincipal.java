8185f08d16f3754ba41913b14127128404f3f6b1
brian <briancisterna@gmail.com>
2026-09-11 19:28:44 -0300

    ahora si arreglo ya que se hizo un quilombo con el push

--- src/presentacion/VentanaPrincipal.java
+++ src/presentacion/VentanaPrincipal.java
@@ -95,7 +95,7 @@ public class VentanaPrincipal {
 				labelCelda.setFont(new Font("SansSerif", Font.BOLD, 16));
 
 				celdas[i][j] = labelCelda;
-				this.panelTablero.add(labelCelda);
+				panelTablero.add(labelCelda);
 			}
 		}
 	}
@@ -116,6 +116,7 @@ public class VentanaPrincipal {
 		btnReiniciarJuego.setFocusable(false);
 
 		btnReiniciarJuego.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				juego.nuevoJuego();
 				actualizarTablero();
@@ -126,19 +127,20 @@ public class VentanaPrincipal {
 	}
 
 	private void crearBotonSugerenciaProximaJugada() {
-		this.btnSugerenciaProxJugada = new JButton("Sugerencia...");
-		this.btnSugerenciaProxJugada.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		this.btnSugerenciaProxJugada.setBounds(393, 40, 145, 23);
-		this.btnSugerenciaProxJugada.setFocusable(false);
+		btnSugerenciaProxJugada = new JButton("Sugerencia...");
+		btnSugerenciaProxJugada.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnSugerenciaProxJugada.setBounds(393, 40, 145, 23);
+		btnSugerenciaProxJugada.setFocusable(false);
 
-		this.btnSugerenciaProxJugada.addActionListener(new ActionListener() {
+		btnSugerenciaProxJugada.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				Direccion direccion = juego.obtenerSugerencia();
 				mostrarSugerencia(direccion);
 			}
 		});
 
-		this.frmThrees.getContentPane().add(this.btnSugerenciaProxJugada);
+		frmThrees.getContentPane().add(btnSugerenciaProxJugada);
 	}
 
 	private void mostrarSugerencia(Direccion direccion) {
@@ -176,37 +178,38 @@ public class VentanaPrincipal {
 	}
 
 	private void crearLabelProximoNumero() {
-		this.lblProximoNumero = new JLabel("Próximo");
-		this.lblProximoNumero.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		this.lblProximoNumero.setBounds(253, 17, 88, 14);
+		lblProximoNumero = new JLabel("Próximo");
+		lblProximoNumero.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblProximoNumero.setBounds(253, 17, 88, 14);
 
-		this.frmThrees.getContentPane().add(this.lblProximoNumero);
+		frmThrees.getContentPane().add(lblProximoNumero);
 	}
 
 	private void crearBotonTablaPosiciones() {
-		this.btnTablaPosiciones = new JButton("Tabla de posiciones");
-		this.btnTablaPosiciones.setHorizontalTextPosition(SwingConstants.CENTER);
-		this.btnTablaPosiciones.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		this.btnTablaPosiciones.setBounds(24, 42, 171, 21);
-		this.btnTablaPosiciones.setFocusable(false);
+		btnTablaPosiciones = new JButton("Tabla de posiciones");
+		btnTablaPosiciones.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnTablaPosiciones.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnTablaPosiciones.setBounds(24, 42, 171, 21);
+		btnTablaPosiciones.setFocusable(false);
 
-		this.btnTablaPosiciones.addActionListener(new ActionListener() {
+		btnTablaPosiciones.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				mostrarTablaDePosiciones();
 			}
 		});
 
-		frmThrees.getContentPane().add(this.btnTablaPosiciones);
+		frmThrees.getContentPane().add(btnTablaPosiciones);
 	}
 
 	private void crearPanelTableroPrincipal() {
-		this.panelTablero = new JPanel();
-		this.panelTablero.setBorder(new EmptyBorder(10, 10, 10, 10));
-		this.panelTablero.setBackground(new Color(207, 229, 222));
-		this.panelTablero.setBounds(75, 74, 423, 290);
-		this.panelTablero.setLayout(new GridLayout(4, 4, 8, 8));
+		panelTablero = new JPanel();
+		panelTablero.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panelTablero.setBackground(new Color(207, 229, 222));
+		panelTablero.setBounds(75, 74, 423, 290);
+		panelTablero.setLayout(new GridLayout(4, 4, 8, 8));
 
-		frmThrees.getContentPane().add(this.panelTablero);
+		frmThrees.getContentPane().add(panelTablero);
 	}
 
 	private void crearLabelInstruccionesParaJugar() {
@@ -219,21 +222,21 @@ public class VentanaPrincipal {
 	}
 
 	private void crearVentanaPrincipal() {
-		this.frmThrees = new JFrame();
-		this.frmThrees.setFocusable(true);
-		this.frmThrees.setResizable(false);
-		this.frmThrees.setTitle("Threes!");
-		this.frmThrees.setBounds(100, 100, 594, 446);
-		this.frmThrees.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmThrees = new JFrame();
+		frmThrees.setFocusable(true);
+		frmThrees.setResizable(false);
+		frmThrees.setTitle("Threes!");
+		frmThrees.setBounds(100, 100, 594, 446);
+		frmThrees.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		this.frmThrees.addKeyListener(new KeyAdapter() {
+		frmThrees.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				moverSegunTecla(e);
 			}
 		});
 
-		this.frmThrees.getContentPane().setLayout(null);
+		frmThrees.getContentPane().setLayout(null);
 	}
 
 	private void actualizarTablero() {
